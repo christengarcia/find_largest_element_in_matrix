b714b5d7aa4db1c64ea8f6b76b6b9164e402f711
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-17 22:47:46 +1100

    Initial commit

---
+++ find_largest_element_in_matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * C# Program to Find Largest Element in a Matrix
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace find_largest_element_in_matrix
+{
+    class ArraySample
+    {
+        int[,] x;
+        ArraySample()
+        {
+            x = new int[,] { { 12, 21, 63 }, { 40, 15, 6 } };
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
